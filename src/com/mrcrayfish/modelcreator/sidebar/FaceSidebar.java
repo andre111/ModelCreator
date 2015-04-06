faff0cb8445a2d7d9ec3adf45be39cecd3465653
Andre Schweiger <andre.spiele@yahoo.de>
2015-04-06 15:04:35 +0200

    Add support for transparency in sidebar

--- src/com/mrcrayfish/modelcreator/sidebar/FaceSidebar.java
+++ src/com/mrcrayfish/modelcreator/sidebar/FaceSidebar.java
@@ -93,6 +93,8 @@ public class FaceSidebar implements Sidebar
 	public void drawSideView(double x, double y, double size, int side, Font font) {
 		GL11.glPushMatrix();
 		{
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			glTranslated(x, y, 0);
 			glLineWidth(2F);
 			
@@ -222,6 +224,7 @@ public class FaceSidebar implements Sidebar
 					glEnd();
 				}
 			}
+			GL11.glDisable(GL11.GL_BLEND);
 		}
 		GL11.glPopMatrix();
 		
