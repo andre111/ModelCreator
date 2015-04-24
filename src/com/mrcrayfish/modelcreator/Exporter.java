c370f8912c04b59e460848b9433c3f7c984d70b0
Andre Schweiger <andre.spiele@yahoo.de>
2015-04-24 19:18:43 +0200

    Convert exporter to use GSON

--- src/com/mrcrayfish/modelcreator/Exporter.java
+++ src/com/mrcrayfish/modelcreator/Exporter.java
@@ -1,12 +1,12 @@
 package com.mrcrayfish.modelcreator;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gson.stream.JsonWriter;
 import com.mrcrayfish.modelcreator.element.Element;
 import com.mrcrayfish.modelcreator.element.ElementManager;
 import com.mrcrayfish.modelcreator.element.Face;
@@ -35,13 +35,14 @@ public class Exporter
 	
 	public File writeJSONFile(File file) {
 		FileWriter fw;
-		BufferedWriter writer;
+		JsonWriter writer;
 		try {
 			if (!file.exists()) {
 				file.createNewFile();
 			}
 			fw = new FileWriter(file);
-			writer = new BufferedWriter(fw);
+			writer = new JsonWriter(fw);
+			writer.setIndent("\t");
 			writeComponents(writer, manager);
 			writer.close();
 			fw.close();
@@ -66,152 +67,124 @@ public class Exporter
 		}
 	}
 
-	private void writeComponents(BufferedWriter writer, ElementManager manager) throws IOException
+	private void writeComponents(JsonWriter writer, ElementManager manager) throws IOException
 	{
-		writer.write("{");
-		writer.newLine();
-		writer.write(space(1) + "\"__comment\": \"Model generated using MrCrayfish's Model Creator (http://mrcrayfish.com/modelcreator/)\",");
-		writer.newLine();
+		writer.beginObject();
+		
+		writer.name("__comment").value("Model generated using MrCrayfish's Model Creator (http://mrcrayfish.com/modelcreator/)");
 		if (!manager.getAmbientOcc())
 		{
-			writer.write("\"ambientocclusion\": " + manager.getAmbientOcc() + ",");
-			writer.newLine();
+			writer.name("ambientocclusion").value(manager.getAmbientOcc());
 		}
 		writeTextures(writer);
-		writer.newLine();
-		writer.write(space(1) + "\"elements\": [");
+
+		writer.name("elements");
+		writer.beginArray();
 		for (int i = 0; i < manager.getElementCount(); i++)
 		{
-			writer.newLine();
-			writer.write(space(2) + "{");
-			writer.newLine();
+			writer.beginObject();
 			writeElement(writer, manager.getElement(i));
-			writer.newLine();
-			writer.write(space(2) + "}");
-			if (i != manager.getElementCount() - 1)
-				writer.write(",");
+			writer.endObject();
 		}
-		writer.newLine();
-		writer.write(space(1) + "]");
-		writer.newLine();
-		writer.write("}");
+		writer.endArray();
+		
+		writer.endObject();
 	}
 
-	private void writeTextures(BufferedWriter writer) throws IOException
+	private void writeTextures(JsonWriter writer) throws IOException
 	{
-		writer.write(space(1) + "\"textures\": {");
-		writer.newLine();
+		writer.name("textures");
+		writer.beginObject();
 		for (String texture : textureList)
 		{
-			writer.write(space(2) + "\"" + textureList.indexOf(texture) + "\": \"" + texture + "\"");
-			if (textureList.indexOf(texture) != textureList.size() - 1)
-			{
-				writer.write(",");
-			}
-			writer.newLine();
+			writer.name(""+textureList.indexOf(texture)).value(texture);
 		}
-		writer.write(space(1) + "},");
+		writer.endObject();
 	}
 
-	private void writeElement(BufferedWriter writer, Element cuboid) throws IOException
+	private void writeElement(JsonWriter writer, Element cuboid) throws IOException
 	{
-		writer.write(space(3) + "\"name\": \"" + cuboid.toString() + "\",");
-		writer.newLine();
+		writer.name("name").value(cuboid.toString());
 		writeBounds(writer, cuboid);
-		writer.newLine();
+
 		if (!cuboid.isShaded())
 		{
-			writeShade(writer, cuboid);
-			writer.newLine();
+			writer.name("shade").value(cuboid.isShaded());
 		}
 		if (cuboid.getRotation() != 0)
 		{
 			writeRotation(writer, cuboid);
-			writer.newLine();
 		}
 		writeFaces(writer, cuboid);
 
 	}
 
-	private void writeBounds(BufferedWriter writer, Element cuboid) throws IOException
+	private void writeBounds(JsonWriter writer, Element cuboid) throws IOException
 	{
-		writer.write(space(3) + "\"from\": [ " + cuboid.getStartX() + ", " + cuboid.getStartY() + ", " + cuboid.getStartZ() + " ], ");
-		writer.newLine();
-		writer.write(space(3) + "\"to\": [ " + (cuboid.getStartX() + cuboid.getWidth()) + ", " + (cuboid.getStartY() + cuboid.getHeight()) + ", " + (cuboid.getStartZ() + cuboid.getDepth()) + " ], ");
+		writer.name("from");
+		writer.beginArray();
+		writer.value(cuboid.getStartX());
+		writer.value(cuboid.getStartY());
+		writer.value(cuboid.getStartZ());
+		writer.endArray();
+		
+		writer.name("to");
+		writer.beginArray();
+		writer.value(cuboid.getStartX() + cuboid.getWidth());
+		writer.value(cuboid.getStartY() + cuboid.getHeight());
+		writer.value(cuboid.getStartZ() + cuboid.getDepth());
+		writer.endArray();
 	}
-
-	private void writeShade(BufferedWriter writer, Element cuboid) throws IOException
+	
+	private void writeRotation(JsonWriter writer, Element cuboid) throws IOException
 	{
-		writer.write(space(3) + "\"shade\": " + cuboid.isShaded() + ",");
-	}
-
-	private void writeRotation(BufferedWriter writer, Element cuboid) throws IOException
-	{
-		writer.write(space(3) + "\"rotation\": { ");
-		writer.write("\"origin\": [ " + cuboid.getOriginX() + ", " + cuboid.getOriginY() + ", " + cuboid.getOriginZ() + " ], ");
-		writer.write("\"axis\": \"" + Element.parseAxis(cuboid.getPrevAxis()) + "\", ");
-		writer.write("\"angle\": " + cuboid.getRotation());
+		writer.name("rotation");
+		writer.beginObject();
+		
+		writer.name("origin");
+		writer.beginArray();
+		writer.value(cuboid.getOriginX());
+		writer.value(cuboid.getOriginY());
+		writer.value(cuboid.getOriginZ());
+		writer.endArray();
+		
+		writer.name("axis").value(Element.parseAxis(cuboid.getPrevAxis()));
+		writer.name("angle").value(cuboid.getRotation());
 		if (cuboid.shouldRescale())
 		{
-			writer.write(", \"rescale\": " + cuboid.shouldRescale());
+			writer.name("rescale").value(cuboid.shouldRescale());
 		}
-		writer.write(" },");
+		writer.endObject();
 	}
 
-	private void writeFaces(BufferedWriter writer, Element cuboid) throws IOException
+	private void writeFaces(JsonWriter writer, Element cuboid) throws IOException
 	{
-		writer.write(space(3) + "\"faces\": {");
-		writer.newLine();
+		writer.name("faces");
+		writer.beginObject();
 		for (Face face : cuboid.getAllFaces())
 		{
 			if(face.isEnabled()) {
-				writer.write(space(4) + "\"" + Face.getFaceName(face.getSide()) + "\": { ");
-				writer.write("\"texture\": \"#" + textureList.indexOf(face.getTextureLocation() + face.getTextureName()) + "\"");
-				writer.write(", \"uv\": [ " + face.getStartU() + ", " + face.getStartV() + ", " + face.getEndU() + ", " + face.getEndV() + " ]");
+				writer.name(Face.getFaceName(face.getSide()));
+				writer.beginObject();
+				
+				writer.name("texture").value("#" + textureList.indexOf(face.getTextureLocation() + face.getTextureName()));
+				
+				writer.name("uv");
+				writer.beginArray();
+				writer.value(face.getStartU());
+				writer.value(face.getStartV());
+				writer.value(face.getEndU());
+				writer.value(face.getEndV());
+				writer.endArray();
+				
 				if (face.getRotation() > 0)
-					writer.write(", \"rotation\": " + (int) face.getRotation() * 90);
+					writer.name("rotation").value((int) face.getRotation() * 90);
 				if (face.isCullfaced())
-					writer.write(", \"cullface\": \"" + Face.getFaceName(face.getSide()) + "\"");
-				writer.write(" }");
-				if (face.getSide() != cuboid.getLastValidFace())
-				{
-					writer.write(",");
-					writer.newLine();
-				}
+					writer.name("cullface").value(Face.getFaceName(face.getSide()));
+				
+				writer.endObject();
 			}
 		}
-		writer.newLine();
-		writer.write(space(3) + "}");
-	}
-
-	/*private void writeChild(BufferedWriter writer) throws IOException
-	{
-		writer.write("{");
-		writer.newLine();
-		writer.write(space(1) + "\"parent\": \"block/" + modelName + "\",");
-		writer.newLine();
-		writer.write(space(1) + "\"textures\": {");
-		writer.newLine();
-		for (int i = 0; i < textureList.size(); i++)
-		{
-			writer.write(space(2) + "\"" + i + "\": \"block/" + textureList.get(i) + "\"");
-			if (i != textureList.size() - 1)
-			{
-				writer.write(",");
-			}
-			writer.newLine();
-		}
-		writer.write(space(1) + "}");
-		writer.write("}");
-	}*/
-
-	private String space(int size)
-	{
-		String space = "";
-		for (int i = 0; i < size; i++)
-		{
-			space += "    ";
-		}
-		return space;
+		writer.endObject();
 	}
 }
